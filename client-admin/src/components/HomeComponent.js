3a87fd8dbd2c5d279d46ace3e62778814702c6a4
quancute4444 <128352357+quancute4444@users.noreply.github.com>
2023-11-21 20:50:18 +0700

    Add files via upload

---
+++ client-admin/src/components/HomeComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="align-center">
+        <h2 className="text-center">Văn Lang IOS</h2>
+        <img src="https://static2-images.vnncdn.net/files/publish/2023/2/26/iphone-15-ultra-194.gif" width="800px" height="600px" alt="" />
+      </div>
+    );
+  }
+}
+export default Home;
---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.kubde5a.mongodb.net',
+    DB_USER: 'QuangQuang',
+    DB_PASS: '0969749419',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: '0969749419',
+    JWT_EXPIRES: '36000000', // in milliseconds
+    EMAIL_USER: 'tranq657@gmail.com', // gmail service
+    EMAIL_PASS: 'rcnnndtsmuxwuobd'
+  };
+  module.exports = MyConstants;
